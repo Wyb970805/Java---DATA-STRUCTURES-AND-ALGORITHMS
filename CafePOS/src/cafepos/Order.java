4172af95dd5e393a39ab6aac4b9a19bd641a5c64
wenloo0411 <wenloo0411@users.noreply.github.com>
2021-02-02 17:07:13 +0800

    Open new class

---
+++ CafePOS/src/cafepos/Order.java
@@ -0,0 +1,18 @@
+/*
+
+ */
+package cafepos;
+
+/**
+ *
+ * @author Wen
+ */
+public class Order {
+    String OrderNum;
+    Item itemList[];
+    int iQty;
+    char type;
+    int tableNo;
+    String staffIncharge;
+    
+}
