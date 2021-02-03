3d9085950d073261deaee86ffcdef9e01e86f745
OEM <OEM@LAPTOP-K2PJVKI2>
2021-02-03 19:12:07 +0800

    create class

---
+++ CafePOS/src/cafepos/Payment.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cafepos;
+
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author OEM
+ */
+public class Payment {
+    
+    String paymentID;
+    float paymentAmount;
+    String paymentMethod;
+    LocalDateTime paid;    //https://www.w3schools.com/java/java_date.asp
+    String paymentType;
+    Order orderList[];
+}
