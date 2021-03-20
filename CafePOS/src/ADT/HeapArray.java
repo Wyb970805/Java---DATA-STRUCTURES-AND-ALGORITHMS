b90381701146f8a11bb085ed2d79782ccc6badce
Chase Tan <Chase Tan@192.168.56.1>
2021-03-20 15:23:17 +0800

    Added incompleted adt specs

--- CafePOS/src/ADT/HeapArray.java
+++ CafePOS/src/ADT/HeapArray.java
@@ -1,57 +1,95 @@
 package ADT;
-
+import java.util.Scanner;
 /**
  *
  * @author Chase Tan
  */
+
+//Wrong thing I did, top forever is top, btm forever is btm, cannot change its value
 public class HeapArray<S> implements HeapInterface<S> {
     
-  private S[] array; //make  a array  variable
-  private int topIndex; // index of top entry
+  private S[] arrayVar; //make  a array  variable
+  private int topIndexHeap; // index of top entry
+  private int btmIndexHeap; // index of btm entry
   private static final int DEFAULT_CAPACITY = 50; // make the limit of stack
+  private int size = heapCount();
 
-  public HeapArray() {
-    this(DEFAULT_CAPACITY); // this line cannot understand
-  }
+    public HeapArray() {
+      this(DEFAULT_CAPACITY); // this line cannot understand
+    }
 
-  public HeapArray(int initialCapacity) {
-    array = (S[]) new Object[initialCapacity]; 
-    topIndex = -1;
-  }
+    public HeapArray(int initialCapacity) {
+      arrayVar = (S[]) new Object[initialCapacity]; 
+      topIndexHeap = -1;
+    }
   
     @Override
     public void pushBottom(S promptResult) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public S popIndex() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ++topIndexHeap;
+        
+        arrayVar[topIndexHeap] = promptResult;
     }
 
     @Override
     public S popTop() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        S topContent = null;
+        
+        if(!isEmpty()) {
+            topContent = arrayVar[topIndexHeap];
+            arrayVar[topIndexHeap] = null;
+            topIndexHeap--;
+        }
+        
+        return topContent;
     }
 
     @Override
     public S peekTop() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        S topContent = null;
+        
+        if(!isEmpty()) {
+            topContent = arrayVar[topIndexHeap];
+        }
+        
+        return topContent;
     }
 
     @Override
-    public S peekIndex() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public S peekIndex(int enteredIndex) {
+        S peekContent = null;
+        
+        if(!isEmpty()) {
+            for (int i=0; i < size; i++) {
+                if(i == enteredIndex) {
+                    peekContent = arrayVar[i];
+                }
+            }
+        }
+        
+        return peekContent;
     }
 
     @Override
     public void clearAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Scanner scanner = new Scanner(System.in);
+        String ans = null;
+        
+        while(ans != "y" || ans != "n" || ans != "Y" || ans != "N") {
+            System.out.print("are you sure you want to clear heap array (Y or N) ?");
+            ans = scanner.nextLine();
+            
+            if(ans == "Y" || ans == "y") {
+                arrayVar = null;
+            }
+        }
+        
     }
 
     @Override
     public boolean isEmpty() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        return topIndexHeap < 0; // It will return false or true base on this statement
     }
 
     @Override
@@ -59,9 +97,13 @@ public class HeapArray<S> implements HeapInterface<S> {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @Override
-    public int count() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public int heapCount() {
+        
+        while(arrayVar != null) {
+            size++;
+        }
+        
+        return size;
     }
   
 }
